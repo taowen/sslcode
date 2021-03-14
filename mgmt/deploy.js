6e52981439a5a72fa66efdd9d525e026fa1a87c9
Tao Wen <taowen@chengfayun.com>
2021-03-14 22:03:07 +0800

    terminate and deploy

---
+++ mgmt/deploy.js
@@ -0,0 +1,192 @@
+'use strict';
+
+const tencentcloud = require('tencentcloud-sdk-nodejs');
+const CbsClient = tencentcloud.cbs.v20170312.Client;
+const cbsClient = new CbsClient({
+    credential: require('./credential'),
+    region: 'ap-bangkok',
+    profile: {
+        httpProfile: {
+            endpoint: 'cbs.tencentcloudapi.com',
+        },
+    },
+});
+const CvmClient = tencentcloud.cvm.v20170312.Client;
+const cvmClient = new CvmClient({
+    credential: require('./credential'),
+    region: 'ap-bangkok',
+    profile: {
+        httpProfile: {
+            endpoint: 'cvm.tencentcloudapi.com',
+        },
+    },
+});
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function findSnapshot() {
+    const snapshots = (
+        await cbsClient.DescribeSnapshots({
+            Filters: [
+                {
+                    Name: 'snapshot-name',
+                    Values: ['sslcode'],
+                },
+            ],
+        })
+    ).SnapshotSet;
+    if (snapshots.length === 0) {
+        throw new Error('sslcode snapshot not found');
+    }
+    console.log('found snapshot', snapshots[0]);
+    return snapshots[0];
+}
+
+async function assertNoDisk() {
+    const disks = (
+        await cbsClient.DescribeDisks({
+            Filters: [
+                {
+                    Name: 'disk-name',
+                    Values: ['sslcode'],
+                },
+            ],
+        })
+    ).DiskSet;
+    if (disks.length !== 0) {
+        console.log('sslcode disk has already been created', disks);
+        throw new Error('sslcode disk has already been created');
+    }
+}
+
+async function createDisk(snapshot) {
+    await assertNoDisk();
+    const result = await cbsClient.CreateDisks({
+        DiskChargeType: 'POSTPAID_BY_HOUR',
+        DiskCount: 1,
+        DiskName: 'sslcode',
+        DiskSize: 20,
+        DiskType: 'CLOUD_SSD',
+        Placement: {
+            ProjectId: 0,
+            Zone: 'ap-bangkok-1',
+        },
+        SnapshotId: snapshot.SnapshotId,
+        Shareable: false,
+    });
+    console.log('create disks', result);
+    while (true) {
+        await sleep(500);
+        const disks = (
+            await cbsClient.DescribeDisks({
+                DiskIds: [result.DiskIdSet[0]],
+            })
+        ).DiskSet;
+        console.log(disks[0].DiskId, disks[0].DiskState);
+        if (disks[0].DiskState !== 'CREATING') {
+            return disks[0];
+        }
+    }
+}
+
+async function assertNoInstance() {
+    const instances = (
+        await cvmClient.DescribeInstances({
+            Filters: [
+                {
+                    Name: 'instance-name',
+                    Values: ['sslcode'],
+                },
+            ],
+        })
+    ).InstanceSet;
+    if (instances.length !== 0) {
+        console.log('sslcode instance has already been created', instances);
+        throw new Error('sslcode instance has already been created');
+    }
+}
+
+async function createInstance() {
+    await assertNoInstance();
+    const result = await cvmClient.RunInstances({
+        InstanceChargeType: 'SPOTPAID',
+        Placement: {
+            Zone: 'ap-bangkok-1',
+            ProjectId: 0,
+        },
+        InstanceMarketOptions: {
+            SpotOptions: {
+                MaxPrice: '1000',
+            },
+        },
+        VirtualPrivateCloud: {
+            AsVpcGateway: false,
+            VpcId: 'vpc-r5t1fjpq',
+            SubnetId: 'subnet-rtek5vp5',
+            Ipv6AddressCount: 0,
+        },
+        InstanceType: 'S2.SMALL1',
+        ImageId: 'img-efriekq8',
+        SystemDisk: {
+            DiskSize: 50,
+            DiskType: 'CLOUD_PREMIUM',
+        },
+        InternetAccessible: {
+            InternetMaxBandwidthOut: 100,
+            PublicIpAssigned: true,
+            InternetChargeType: 'TRAFFIC_POSTPAID_BY_HOUR',
+        },
+        InstanceName: 'sslcode',
+        TagSpecification: [
+            {
+                ResourceType: 'instance',
+                Tags: [{ Key: 'sslcode', Value: 'true' }],
+            },
+        ],
+        LoginSettings: {
+            KeepImageLogin: 'true',
+        },
+        SecurityGroupIds: ['sg-fwu447qy'],
+        InstanceCount: 1,
+        EnhancedService: {
+            SecurityService: {
+                Enabled: true,
+            },
+            MonitorService: {
+                Enabled: true,
+            },
+        },
+    });
+    console.log('run instance', result);
+    while (true) {
+        await sleep(500);
+        const instances = (
+            await cvmClient.DescribeInstances({
+                InstanceIds: [result.InstanceIdSet[0]],
+            })
+        ).InstanceSet;
+        console.log(instances[0].InstanceId, instances[0].InstanceState);
+        if (instances[0].InstanceState !== 'PENDING') {
+            return instances[0];
+        }
+    }
+}
+
+async function attachDisk(disk, instance) {
+    const result = await cbsClient.AttachDisks({
+        DiskIds: [disk.DiskId],
+        InstanceId: instance.InstanceId,
+    });
+    console.log('attach disk', result);
+}
+
+exports.main_handler = async (event, context) => {
+    const snapshot = await findSnapshot();
+    const disk = await createDisk(snapshot);
+    const instance = await createInstance();
+    await attachDisk(disk, instance);
+};
+
+exports.main_handler();
---
+++ mgmt/terminate.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const tencentcloud = require('tencentcloud-sdk-nodejs');
+
+const CbsClient = tencentcloud.cbs.v20170312.Client;
+const cbsClient = new CbsClient({
+    credential: require('./credential'),
+    region: 'ap-bangkok',
+    profile: {
+        httpProfile: {
+            endpoint: 'cbs.tencentcloudapi.com',
+        },
+    },
+});
+const CvmClient = tencentcloud.cvm.v20170312.Client;
+const cvmClient = new CvmClient({
+    credential: require('./credential'),
+    region: 'ap-bangkok',
+    profile: {
+        httpProfile: {
+            endpoint: 'cvm.tencentcloudapi.com',
+        },
+    },
+});
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+
+async function archiveOldSnapshots(newSnapshot) {
+    const snapshots = (await cbsClient.DescribeSnapshots({
+        Filters: [{
+            Name: 'snapshot-name',
+            Values: ['sslcode']
+        }]
+    })).SnapshotSet;
+    for (const snapshot of snapshots) {
+        if (newSnapshot.SnapshotId === snapshot.SnapshotId) {
+            continue;
+        }
+        const result = await cbsClient.ModifySnapshotAttribute({
+            SnapshotId: snapshot.SnapshotId,
+            SnapshotName: 'archival',
+            IsPermanent: false,
+            Deadline: new Date(new Date().getTime() + 3600 * 24 * 2 * 1000).toISOString().replace('T', ' ').substr(0, 19)
+        });
+        console.log('modify snapshot', result);
+    }
+}
+
+async function createSnapshot(disk) {
+    const result = await cbsClient.CreateSnapshot({
+        DiskId: disk.DiskId,
+        SnapshotName: 'sslcode',
+    });
+    console.log('create snapshot', result);
+    while (true) {
+        await sleep(500);
+        const snapshots = (
+            await cbsClient.DescribeSnapshots({
+                SnapshotIds: [result.SnapshotId],
+            })
+        ).SnapshotSet;
+        console.log(snapshots[0].SnapshotId, snapshots[0].SnapshotState);
+        if (snapshots[0].SnapshotState !== 'CREATING') {
+            return snapshots[0];
+        }
+    }
+}
+
+async function getInstance() {
+    const instances = (
+        await cvmClient.DescribeInstances({
+            Filters: [
+                {
+                    Name: 'instance-name',
+                    Values: ['sslcode'],
+                },
+            ],
+        })
+    ).InstanceSet;
+    if (instances.length !== 1) {
+        console.log('sslcode instance has not been created', instances);
+        throw new Error('sslcode instance has not been created');
+    }
+    return instances[0];
+}
+
+async function terminateInstance() {
+    const instance = await getInstance();
+    const result = await cvmClient.TerminateInstances({
+        InstanceIds: [instance.InstanceId],
+    });
+    console.log('terminate instance', result);
+    while (true) {
+        await sleep(1500);
+        const instances = (
+            await cvmClient.DescribeInstances({
+                Filters: [
+                    {
+                        Name: 'instance-name',
+                        Values: ['sslcode'],
+                    },
+                ],
+            })
+        ).InstanceSet;
+        if (instances.length !== 0) {
+            console.log(instances[0].InstanceId, instances[0].InstanceState);
+        } else {
+            break;
+        }
+    }
+}
+
+async function getDisk() {
+    let disks = (
+        await cbsClient.DescribeDisks({
+            Filters: [
+                {
+                    Name: 'disk-name',
+                    Values: ['sslcode'],
+                },
+            ],
+        })
+    ).DiskSet;
+    disks = disks.filter(disk => disk.DiskName === 'sslcode');
+    if (disks.length !== 1) {
+        console.log('sslcode disk has not been created', disks);
+        throw new Error('sslcode disk has not been created');
+    }
+    return disks[0];
+}
+
+async function terminateDisk(disk) {
+    const result = await cbsClient.TerminateDisks({
+        DiskIds: [disk.DiskId],
+    });
+    console.log('terminate disk', result);
+}
+
+exports.main_handler = async (event, context) => {
+    const disk = await getDisk();
+    const newSnapshot = await createSnapshot(disk);
+    await archiveOldSnapshots(newSnapshot);
+    await terminateInstance();
+    await terminateDisk(disk);
+};
+
+exports.main_handler();
